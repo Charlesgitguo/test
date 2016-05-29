654f7d80f3117e8dd6291eb00ac13399ad58c969
qqguo <qqguo2016@163.com>
2016-05-29 20:57:29 +0800

    first commit

---
+++ QuartzOne/src/test/HelloJob.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class HelloJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Quartz! - executing its JOB at "+ 
+	            new Date() + " by " + context.getJobDetail().getKey().getName()+" ,"+
+				context.getTrigger().getKey().getName());
+	}
+
+}
